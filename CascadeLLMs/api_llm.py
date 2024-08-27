553b7e30b7cb4b86c0c4fed782638044194f327f
wesar <1982047268@qq.com>
2024-08-27 20:03:36 +0800

    ollama集成

--- CascadeLLMs/api_llm.py
+++ CascadeLLMs/api_llm.py
@@ -1,5 +1,6 @@
 import openai
 from openai import OpenAI
+import ollama
 import threading
 import yaml
 from langchain.prompts import PromptTemplate
@@ -7,31 +8,44 @@ import time
 
         
 class PromptGenerator:
-    def __init__(self, 
-                 api_set: dict, 
+    def __init__(self,
+                 api_in_use: str,
+                 openai_api_set: dict,
+                 ollama_api_set: dict, 
                  style_template: list[dict],
                  sys_ins: str):
         
         self.sys_ins = sys_ins
+        self.api_in_use = api_in_use
         self.threads = []
         self.Lock = threading.Lock()
         
         self.result_dict: dict[str, str] = {}
         self.full_process: dict[str, list] = {}
         
-        self.init_openai_api(api_set)
+        if api_in_use == "openai":
+            self.init_openai_api(openai_api_set)
+        elif api_in_use =="ollama":
+            self.init_ollama_api(ollama_api_set)
+            
         self.load_style_template(style_template)
-        pass
     
     def init_openai_api(self, api_set: dict):
-        self.api_key = api_set['api_key']
-        self.base_url = api_set['base_url']
-        self.model = api_set['model']
+        # self.api_key = api_set['api_key']
+        # self.base_url = api_set['base_url']
+        # self.model = api_set['model']
+        self.__dict__.update(api_set)
         openai._client = OpenAI(
             api_key=self.api_key, # 替换成真实的API_KEY
             base_url=self.base_url, # 服务endpoint
         )
-        pass
+    
+    def init_ollama_api(self, api_set: dict):
+        self.__dict__.update(api_set)
+        ollama._client = ollama.Client(
+            host=f"http://{self.host}:{self.port}"
+        )
+        
     
     def load_style_template(self, style_template: list[dict]):
         self.prompt_template_dict = {}
@@ -39,7 +53,7 @@ class PromptGenerator:
             self.prompt_template_dict[style['name']] = style['template']
         pass
     
-    def single_response(self, prompt: str)->str:
+    def single_response_openai(self, prompt: str)->str:
         client = OpenAI(
             api_key=self.api_key,
             base_url=self.base_url,
@@ -54,13 +68,44 @@ class PromptGenerator:
         )
         return completion.choices[0].message.content
     
-    def multi_turn_response(self, raw_input: str, style_name: str, prompt_list: list[PromptTemplate])->str:
+    def single_response_ollama(self, prompt: str)->str:
+        client = ollama.Client(
+            host=f"http://{self.host}:{self.port}"
+        )
+    
+        res = client.chat(
+            model=self.model,
+            stream=False,
+            messages=[
+                {"role": "system", "content": self.sys_ins},
+                {"role": "user", "content": prompt}
+            ],
+            options={"temperature": self.temperature}
+        )
+        return res['message']['content']
+    
+    def multi_turn_response_openai(self, raw_input: str, style_name: str, prompt_list: list[PromptTemplate])->str:
         input_var = {'input': raw_input}
         self.full_process[style_name] = []
         
         for i, temp in enumerate(prompt_list):
             prompt = temp.format(**input_var)
-            res = self.single_response(prompt)
+            res = self.single_response_openai(prompt)
+            input_var = {'input': res}
+            
+            self.full_process[style_name].append(
+                {f"turn {i+1}": {"prompt": prompt, "response": res}}
+            )
+        
+        return res
+    
+    def multi_turn_response_ollama(self, raw_input: str, style_name: str, prompt_list: list[PromptTemplate])->str:
+        input_var = {'input': raw_input}
+        self.full_process[style_name] = []
+        
+        for i, temp in enumerate(prompt_list):
+            prompt = temp.format(**input_var)
+            res = self.single_response_ollama(prompt)
             input_var = {'input': res}
             
             self.full_process[style_name].append(
@@ -74,7 +119,10 @@ class PromptGenerator:
         self.full_process = {}
         
         for style_name, temp_list in self.prompt_template_dict.items():
-            tmp_thread = ResponseThread(style_name, raw_input, temp_list, self)
+            if self.api_in_use == 'openai':
+                tmp_thread = ResThreadOpenai(style_name, raw_input, temp_list, self)
+            elif self.api_in_use == 'ollama':
+                tmp_thread = ResThreadOllama(style_name, raw_input, temp_list, self)
             tmp_thread.start()
             self.threads.append(tmp_thread)
             
@@ -84,7 +132,7 @@ class PromptGenerator:
         return self.result_dict, self.full_process
         
 
-class ResponseThread(threading.Thread):
+class ResThreadOpenai(threading.Thread):
     def __init__(self, style: str, raw_input: str, prompt_list: list, 
                  generator: PromptGenerator):
         threading.Thread.__init__(self)
@@ -95,7 +143,26 @@ class ResponseThread(threading.Thread):
  
     def run(self):
         print(f"元素[{self.style}]开始", self.name)
-        res = self.generator.multi_turn_response(self.raw_input, self.style, self.prompt_list)
+        res = self.generator.multi_turn_response_openai(self.raw_input, self.style, self.prompt_list)
+        self.generator.Lock.acquire()
+        self.generator.result_dict[self.style] = res
+        self.generator.Lock.release()
+        
+    def __del__(self):
+        print(f"元素[{self.style}]结束", self.name)
+        
+class ResThreadOllama(threading.Thread):
+    def __init__(self, style: str, raw_input: str, prompt_list: list, 
+                 generator: PromptGenerator):
+        threading.Thread.__init__(self)
+        self.style = style
+        self.raw_input = raw_input
+        self.prompt_list = prompt_list
+        self.generator = generator
+ 
+    def run(self):
+        print(f"元素[{self.style}]开始", self.name)
+        res = self.generator.multi_turn_response_ollama(self.raw_input, self.style, self.prompt_list)
         self.generator.Lock.acquire()
         self.generator.result_dict[self.style] = res
         self.generator.Lock.release()
@@ -110,9 +177,11 @@ if __name__=="__main__":
     with open("/home/roo/dream/wutr/mprompt-encoder/config/templates.yml", 'r') as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     styles = data['styles']
-    api_set = data['api_set']
+    api_in_use = data['api_in_use']
+    ollama_api_set = data['ollama_api_set']
+    openai_api_set = data['openai_api_set']
     sys_ins = data['sys_ins']
-    gener = PromptGenerator(api_set, styles, sys_ins)
+    gener = PromptGenerator(api_in_use, openai_api_set, ollama_api_set, styles, sys_ins)
     res, his = gener.multi_thread_response("一个红色的苹果")
     # print(res.keys())
     # print(his)
